5136454746f44746d1be402f6afbcc29300995a2
JoeM10 <joemcdaniel1592@gmail.com>
2026-06-17 22:13:14 -0700

    Implemented Swagger into the API stack for documentation. Completed all of the paths and definitions for current routes.

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,23 @@
 from flask import Flask
-from .extensions import ma, limiter, cache
-from .models import db
-from .blueprints.customers import customers_bp
-from .blueprints.mechanics import mechanics_bp
-from .blueprints.service_tickets import service_tickets_bp
-from .blueprints.inventory import inventory_bp
-from .blueprints.admin import admin_bp
+from app.extensions import ma, limiter, cache
+from app.models import db
+from app.blueprints.customers import customers_bp
+from app.blueprints.mechanics import mechanics_bp
+from app.blueprints.service_tickets import service_tickets_bp
+from app.blueprints.inventory import inventory_bp
+from app.blueprints.admin import admin_bp
+from flask_swagger_ui import get_swaggerui_blueprint
+
+SWAGGER_URL = '/api/docs'  # URL for exposing Swagger UI (without trailing '/')
+API_URL = '/static/swagger.yaml'  # Our API URL (can of course be a local resource)
+
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Your API's Name"
+    }
+)
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -23,5 +35,6 @@ def create_app(config_name):
     app.register_blueprint(service_tickets_bp, url_prefix="/service-tickets")
     app.register_blueprint(inventory_bp, url_prefix="/inventory")
     app.register_blueprint(admin_bp, url_prefix="/admin")
+    app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL) #Registering our swagger blueprint
     
     return app
